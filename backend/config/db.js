1489bb26e2cd3f5c5cef14b53d3bae4f4cc2af58
Rahul Dhakar <rahuldhakarmm@gmail.com>
2026-03-29 01:20:25 +0530

    pushing local changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,11 +2,18 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
+        if (mongoose.connection.readyState >= 1) {
+            console.log('MongoDB is already connected.');
+            return;
+        }
+        if (!process.env.MONGO_URI) {
+            console.error('Missing MONGO_URI string');
+            return;
+        }
         const conn = await mongoose.connect(process.env.MONGO_URI);
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.error(`Error: ${error.message}`);
-        process.exit(1);
     }
 };
 
